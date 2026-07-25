9ebefe521177481c3634901458b2e815215e7237
AnishRN <anish.17175@sakec.ac.in>
2026-07-25 16:09:08 +0530

    Update setup.js

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -1,18 +1,6 @@
-/*
-    Jest setup file.
-
-    This file runs before every test suite.
-
-    Future improvements:
-    - Connect to a dedicated test database
-    - Seed test users
-    - Clean up database after tests
-*/
-
-beforeAll(async () => {
-    console.log("Starting test suite...");
-});
-
-afterAll(async () => {
-    console.log("Finished test suite.");
-});
+module.exports = {
+    testEnvironment: "node",
+    setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
+    testMatch: ["**/tests/**/*.test.js"],
+    verbose: true
+};
