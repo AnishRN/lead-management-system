24cbbdb96812658251a17d525bcafb711aaaabce
AnishRN <anish.17175@sakec.ac.in>
2026-07-25 16:10:08 +0530

    Update auth.test.js

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,29 +1,24 @@
-describe("Authentication API", () => {
-
-    test("should reject invalid login", async () => {
-
-        /*
-            Placeholder test.
-
-            Future implementation:
-            POST /api/auth/login
-            Expect HTTP 401
-        */
-
-        expect(true).toBe(true);
-
+const api = require("./helpers");
+describe("Authentication", () => {
+    test("Login should return JWT token", async () => {
+        const response = await api
+            .post("/api/auth/login")
+            .send({
+                email: "admin@test.com",
+                password: "admin123"
+            });
+        expect([200, 401]).toContain(response.status);
+        if (response.status === 200) {
+           expect(response.body).toHaveProperty("token");
+        }
     });
-
-    test("should deny access without JWT", async () => {
-
-        /*
-            Future implementation:
-            GET /api/auth/me
-            Expect HTTP 401
-        */
-
-        expect(true).toBe(true);
-
+    test("Reject invalid credentials", async () => {
+        const response = await api
+            .post("/api/auth/login")
+            .send({
+                email: "wrong@test.com",
+                password: "wrongpassword"
+            });
+        expect([400,401]).toContain(response.status);
     });
-
 });
